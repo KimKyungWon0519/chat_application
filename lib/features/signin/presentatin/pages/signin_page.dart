b97408e3333e4f6833d0080152364f752d18b6a2
kim kyugnwon <gwon080870@gmail.com>
2024-01-12 10:55:53 +0900

    FEAT: Initialize main

---
+++ lib/features/signin/presentatin/pages/signin_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SigninPage extends StatelessWidget {
+  const SigninPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
