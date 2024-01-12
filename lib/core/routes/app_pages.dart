f4b4d7b88f0e8d772cd68921456e41a3387af065
kim kyugnwon <gwon080870@gmail.com>
2024-01-12 10:54:05 +0900

    FEAT: Create routes

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,7 +1,19 @@
 import 'package:chat_application/core/routes/app_routes.dart';
+import 'package:chat_application/features/signin/presentatin/pages/signin_page.dart';
+import 'package:go_router/go_router.dart';
 
 final class AppPages {
   const AppPages._();
 
   static const String _initializeRoute = AppRoutes.init;
+
+  static final GoRouter routes = GoRouter(
+    routes: [
+      GoRoute(
+        path: AppRoutes.init,
+        builder: (context, state) => const SigninPage(),
+      ),
+    ],
+    initialLocation: _initializeRoute,
+  );
 }
