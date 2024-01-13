b5bf29d72802fab711ba491f7c259bd6eae22d32
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 17:47:52 +0900

    FEAT: Create Signin ViewModel

---
+++ lib/src/features/signin/presentation/presenter/providers.dart
@@ -0,0 +1,4 @@
+import 'package:chat_application/src/features/signin/presentation/presenter/signin_viewmodel.dart';
+import 'package:riverpod/riverpod.dart';
+
+late final Provider<SigninViewModel> signinProvider;
