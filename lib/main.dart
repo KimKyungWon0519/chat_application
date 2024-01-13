c053fbd958d2ca2423534b61c3fa4c4fa2c776cc
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 18:36:10 +0900

    FEAT: Init Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:chat_application/firebase_options.dart';
 import 'package:chat_application/src/core/routes/app_pages.dart';
 import 'package:chat_application/src/dependency_injection.dart' as Di;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseAuth.instanceFor(app: app);
+
   Di.initialize();
 
   runApp(const ProviderScope(
