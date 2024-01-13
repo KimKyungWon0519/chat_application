8822932151baf27b40472a3f7c9fd6ca007e091c
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 19:02:32 +0900

    FEAT: Add firebase auth di

--- lib/src/features/signin/presentation/presenter/signin_viewmodel.dart
+++ lib/src/features/signin/presentation/presenter/signin_viewmodel.dart
@@ -1,4 +1,12 @@
+import 'package:chat_application/src/features/signin/domain/usecase/firebae_auth_usecase.dart';
+
 class SigninViewModel {
+  final FirebaseAuthUseCase _firebaseAuthUseCase;
+
   String email = '';
   String password = '';
+
+  SigninViewModel({
+    required FirebaseAuthUseCase firebaseAuthUseCase,
+  }) : _firebaseAuthUseCase = firebaseAuthUseCase;
 }
