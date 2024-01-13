aa077d5ca27604b8a8d12ba6f175733b5ec7e20d
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 18:53:55 +0900

    FEAT: Sigin on usecase

--- lib/src/features/signin/domain/usecase/firebae_auth_usecase.dart
+++ lib/src/features/signin/domain/usecase/firebae_auth_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:chat_application/src/features/signin/domain/repository/firebase_auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthUseCase {
   final FirebaseAuthRepository _firebaseAuthRepository;
@@ -6,4 +7,8 @@ class FirebaseAuthUseCase {
   const FirebaseAuthUseCase({
     required FirebaseAuthRepository firebaseAuthRepository,
   }) : _firebaseAuthRepository = firebaseAuthRepository;
+
+  Future<UserCredential> signin(String email, String password) {
+    return _firebaseAuthRepository.signin(email, password);
+  }
 }
