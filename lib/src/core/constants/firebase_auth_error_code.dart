3ea32c0d2bd5a78a0d38ffc5f665ec820221a1cc
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 20:10:04 +0900

    FEAT: Create Firebase auth error code constants

---
+++ lib/src/core/constants/firebase_auth_error_code.dart
@@ -0,0 +1,7 @@
+final class FirebaseAuthErrorCode {
+  const FirebaseAuthErrorCode._();
+
+  static String userNotFound = 'user-not-found';
+  static String wrongPassword = 'wrong-password';
+  static String invalidCredential = 'invalid-credential';
+}
--- lib/src/features/signin/presentation/pages/local_widgets/sigin_button.dart
+++ lib/src/features/signin/presentation/pages/local_widgets/sigin_button.dart
@@ -1,9 +1,9 @@
+import 'package:chat_application/src/core/constants/firebase_auth_error_code.dart';
 import 'package:chat_application/src/core/utils/dialogs.dart';
 import 'package:chat_application/src/core/values/exceptions/signin_exception.dart';
 import 'package:chat_application/src/features/signin/presentation/presenter/providers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -36,32 +36,40 @@ class SigninButton extends ConsumerWidget {
     if (formKey.currentState!.validate()) {
       formKey.currentState!.save();
 
-      bool isLoading = true;
+      await _signinEvent(context, ref);
+    }
+  }
 
-      Dialogs.showLoading(context, isLoading);
+  Future<void> _signinEvent(BuildContext context, WidgetRef ref) async {
+    bool isLoading = true;
 
-      try {
-        await ref.read(signinProvider).signin().whenComplete(() {
-          isLoading = false;
-          context.pop();
-        });
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'user-not-found' ||
-            e.code == 'wrong-password' ||
-            e.code == 'invalid-credential') {
-          if (context.mounted) {
-            Dialogs.showError(const InvalidCredentialException(), context);
-          }
-        } else {
-          if (context.mounted) {
-            Dialogs.showError(const UnknownException(), context);
-          }
+    Dialogs.showLoading(context, isLoading);
+
+    try {
+      await ref.read(signinProvider).signin().whenComplete(() {
+        isLoading = false;
+        context.pop();
+      });
+    } on FirebaseAuthException catch (e) {
+      if (_isInvalidCredential(e.code)) {
+        if (context.mounted) {
+          Dialogs.showError(const InvalidCredentialException(), context);
         }
-      } catch (e) {
+      } else {
         if (context.mounted) {
           Dialogs.showError(const UnknownException(), context);
         }
       }
+    } catch (e) {
+      if (context.mounted) {
+        Dialogs.showError(const UnknownException(), context);
+      }
     }
+  }
+
+  bool _isInvalidCredential(String errorCode) {
+    return errorCode == FirebaseAuthErrorCode.userNotFound ||
+        errorCode == FirebaseAuthErrorCode.wrongPassword ||
+        errorCode == FirebaseAuthErrorCode.invalidCredential;
   }
 }
