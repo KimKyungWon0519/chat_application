6d1e2a13ef76b61da5793ca24a326bab86d43ea1
kim kyugnwon <gwon080870@gmail.com>
2024-01-13 18:17:25 +0900

    FEAT: Create validator config

---
+++ lib/src/core/utils/validator.dart
@@ -0,0 +1,14 @@
+import 'package:chat_application/src/constants/regexp_source.dart';
+
+class Validator {
+  const Validator._();
+
+  static String? emailValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'email을 입력해주세요';
+    } else if (!RegExp(RegExpSource.emailRegExp).hasMatch(value)) {
+      return '올바른 email을 입력해주세요.';
+    }
+    return null;
+  }
+}
