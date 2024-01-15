af01b177e7ecbf999b8ba82669096e755e695db3
kim kyugnwon <gwon080870@gmail.com>
2024-01-15 14:15:10 +0900

    FEAT: Create signup methods

--- lib/src/features/auth/domain/repository/firebase_auth_repository.dart
+++ lib/src/features/auth/domain/repository/firebase_auth_repository.dart
@@ -2,4 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class FirebaseAuthRepository {
   Future<UserCredential> signin(String email, String password);
+  Future<UserCredential> signup(String email, String password);
+  Future<void> changeUserName(User user, String name);
+  Future<void> sendEmailVertification(User user);
 }
