a69b19a51784002207a4e7ee5c3d49cd178c7561
kim kyugnwon <gwon080870@gmail.com>
2024-01-16 19:22:03 +0900

    FEAT: Initialize friends page

---
+++ lib/src/features/friends/presentation/pages/friends_page/friends_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FriendsPage extends StatelessWidget {
+  const FriendsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('친구'),
+      ),
+    );
+  }
+}
