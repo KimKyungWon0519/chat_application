1987d5b94bd43d35f53e5a1540d30dbfc0519421
kim kyugnwon <gwon080870@gmail.com>
2024-01-17 15:39:45 +0900

    FEAT: Add Type

--- lib/src/features/friends/presentation/presenter/providers.dart
+++ lib/src/features/friends/presentation/presenter/providers.dart
@@ -1,3 +1,4 @@
+import 'package:chat_application/src/features/friends/presentation/presenter/friends_viewmodel.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-late final Provider friendsProvider;
+late final Provider<FriendsViewModel> friendsProvider;
