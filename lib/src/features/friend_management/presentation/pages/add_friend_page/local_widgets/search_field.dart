9a9879ff2cfb1fd9d868a387daef34127fbd9228
kim kyugnwon <gwon080870@gmail.com>
2024-01-19 03:57:04 +0900

    FEAT: Stl=>Stf on SearchFieldWidget

--- lib/src/features/friend_management/presentation/pages/add_friend_page/local_widgets/search_field.dart
+++ lib/src/features/friend_management/presentation/pages/add_friend_page/local_widgets/search_field.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
   const SearchField({super.key});
 
+  @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
   @override
   Widget build(BuildContext context) {
     return Row(
