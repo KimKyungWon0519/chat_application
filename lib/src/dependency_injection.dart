4d7c22909a89c4d6e23c027f58afc44678447ff3
kim kyugnwon <gwon080870@gmail.com>
2024-01-19 03:51:42 +0900

    FEAT: Initialize friend management initalize

--- lib/src/dependency_injection.dart
+++ lib/src/dependency_injection.dart
@@ -2,8 +2,11 @@ import 'package:chat_application/src/features/auth/dependency_injection.dart'
     as Auth;
 import 'package:chat_application/src/features/friends/dependency_injection.dart'
     as Friends;
+import 'package:chat_application/src/features/friend_management/dependency_injection.dart'
+    as FriendManagement;
 
 void initialize() {
   Auth.initializeDependencyInjection();
   Friends.initializeDependencyInjection();
+  FriendManagement.initializeDependencyInjection();
 }
---
+++ lib/src/features/friend_management/dependency_injection.dart
@@ -0,0 +1,30 @@
+import 'package:chat_application/src/features/friend_management/data/repository/user_store_repository_impl.dart';
+import 'package:chat_application/src/features/friend_management/domain/repository/user_store_repository.dart';
+import 'package:chat_application/src/features/friend_management/domain/usecase/users_store_usecase.dart';
+import 'package:chat_application/src/features/friend_management/presentation/presenter/add_friend_viewmodel.dart';
+import 'package:chat_application/src/features/friend_management/presentation/presenter/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void initializeDependencyInjection() {
+  /* Start Initialize Repository */
+
+  UsersStoreRepository usersStoreRepository = UsersStoreRepositoryImpl();
+
+  /* End Initialize Repository */
+
+  /* Start Initialize UseCase */
+
+  UsersStoreUseCase usersStoreUseCase = UsersStoreUseCase(
+    usersStoreRepository: usersStoreRepository,
+  );
+
+  /* End Initialize UseCase */
+
+  /* Start Initialize ViewModel */
+
+  addFriendProvider = Provider(
+    (ref) => AddFriendViewModel(usersStoreUseCase: usersStoreUseCase),
+  );
+
+  /* End Initialize ViewModel */
+}
