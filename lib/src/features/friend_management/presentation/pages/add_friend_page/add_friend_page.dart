e040d0aeb25131f234c7ed69d6d4ffa76f6c0089
kim kyugnwon <gwon080870@gmail.com>
2024-01-18 21:11:51 +0900

    FEAT: Create AddFriendPage

---
+++ lib/src/features/friend_management/presentation/pages/add_friend_page/add_friend_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddFriendPage extends StatelessWidget {
+  const AddFriendPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('친구 추가'),
+      ),
+    );
+  }
+}
