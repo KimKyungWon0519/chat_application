c24e133ccee2f45183e7b43b08797ca909b235db
kim kyugnwon <gwon080870@gmail.com>
2024-01-19 03:46:01 +0900

    FEAT: Create UsersStoreUseCase

---
+++ lib/src/features/friend_management/domain/usecase/users_store_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:chat_application/src/features/friend_management/domain/repository/user_store_repository.dart';
+
+class UsersStoreUseCase {
+  final UsersStoreRepository _usersStoreRepository;
+
+  const UsersStoreUseCase({
+    required UsersStoreRepository usersStoreRepository,
+  }) : _usersStoreRepository = usersStoreRepository;
+}
