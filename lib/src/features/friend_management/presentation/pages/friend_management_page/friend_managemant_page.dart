816db56196c0fe2127cd0614efa52b0f075266d8
kim kyugnwon <gwon080870@gmail.com>
2024-01-18 20:42:37 +0900

    FAET: Create friend management page

---
+++ lib/src/features/friend_management/presentation/pages/friend_management_page/friend_managemant_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FriendManagementPage extends StatelessWidget {
+  const FriendManagementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('친구 관리'),
+      ),
+    );
+  }
+}
