a6957f96fe13ccb6b3e82e60998feeee1ef879a9
kim kyugnwon <gwon080870@gmail.com>
2024-01-20 17:26:13 +0900

    FEAT: Push AccountManagementPage on click AccountManagementButton

--- lib/src/features/settings/presentation/pages/settings_page/local_widgets/account_management_button.dart
+++ lib/src/features/settings/presentation/pages/settings_page/local_widgets/account_management_button.dart
@@ -1,5 +1,7 @@
+import 'package:chat_application/src/core/routes/app_routes.dart';
 import 'package:chat_application/src/features/settings/presentation/pages/settings_page/local_widgets/management_button.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class AccountManagementButton extends StatelessWidget {
   const AccountManagementButton({super.key});
@@ -8,7 +10,8 @@ class AccountManagementButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ManagementButton(
       name: '계정 설정',
-      onTap: () {},
+      onTap: () =>
+          context.pushNamed(AccountManagementRoutes.accountManagement.name),
     );
   }
 }
