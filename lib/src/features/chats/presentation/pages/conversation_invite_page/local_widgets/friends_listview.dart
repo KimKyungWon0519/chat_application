5bbb48604b56fda21ed1e0e5bfc9e5fdb020c5df
kim kyugnwon <gwon080870@gmail.com>
2024-01-20 21:18:19 +0900

    FEAT: Get userList state

--- lib/src/features/chats/presentation/pages/conversation_invite_page/local_widgets/friends_listview.dart
+++ lib/src/features/chats/presentation/pages/conversation_invite_page/local_widgets/friends_listview.dart
@@ -1,10 +1,15 @@
+import 'package:chat_application/src/features/chats/domain/model/user_info.dart';
+import 'package:chat_application/src/features/chats/presentation/presenter/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FriendsListView extends StatelessWidget {
+class FriendsListView extends ConsumerWidget {
   const FriendsListView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<UserInfo> users = ref.watch(conversationInviteProvider);
+
     return ListView.builder(
       itemBuilder: (context, index) {
         return ListTile(
@@ -16,7 +21,7 @@ class FriendsListView extends StatelessWidget {
           ),
         );
       },
-      itemCount: 100,
+      itemCount: users.length,
     );
   }
 }
