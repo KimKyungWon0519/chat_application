99ae32c3e4d6815aed5156758be4d0e816ea67e6
kim kyugnwon <gwon080870@gmail.com>
2024-01-20 20:59:21 +0900

    FEAT: Initialize ConverstationInvitePage UI

--- lib/src/features/chats/presentation/pages/conversation_invite_page/conversation_invite_page.dart
+++ lib/src/features/chats/presentation/pages/conversation_invite_page/conversation_invite_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'local_widgets/friends_listview.dart';
+import 'local_widgets/search_friend_field.dart';
+import 'local_widgets/selected_count.dart';
+
 class ConversationInvitePage extends StatelessWidget {
   const ConversationInvitePage({super.key});
 
@@ -8,6 +12,18 @@ class ConversationInvitePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('대화 상대 초대'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SearchFriendField(),
+            SelectedCount(),
+            Expanded(
+              child: FriendsListView(),
+            ),
+          ],
+        ),
       ),
     );
   }
