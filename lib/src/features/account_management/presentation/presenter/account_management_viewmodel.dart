1e3c680c0009e68e53085019f4d4628b2ff5dede
kim kyugnwon <gwon080870@gmail.com>
2024-01-20 19:35:10 +0900

    FEAT: Signout on viewmodel

--- lib/src/features/account_management/presentation/presenter/account_management_viewmodel.dart
+++ lib/src/features/account_management/presentation/presenter/account_management_viewmodel.dart
@@ -1,1 +1,13 @@
-class AccountManagementViewModel {}
+import 'package:chat_application/src/features/auth/domain/usecase/firebae_auth_usecase.dart';
+
+class AccountManagementViewModel {
+  final AuthUseCase _authUseCase;
+
+  AccountManagementViewModel({
+    required AuthUseCase authUseCase,
+  }) : _authUseCase = authUseCase;
+
+  Future<void> signout() {
+    return _authUseCase.signout();
+  }
+}
