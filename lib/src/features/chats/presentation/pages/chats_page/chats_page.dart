9110e38c22b24250ac0b73a919c957feed2a99db
kim kyugnwon <gwon080870@gmail.com>
2024-01-20 20:15:23 +0900

    FEAT: Push ConversationInvitePage

--- lib/src/features/chats/presentation/pages/chats_page/chats_page.dart
+++ lib/src/features/chats/presentation/pages/chats_page/chats_page.dart
@@ -1,4 +1,6 @@
+import 'package:chat_application/src/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ChatsPage extends StatelessWidget {
   const ChatsPage({super.key});
@@ -10,7 +12,8 @@ class ChatsPage extends StatelessWidget {
         title: const Text('채팅'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () =>
+                context.pushNamed(ChatsSubRoutes.conversationInvite.name),
             icon: const Icon(Icons.add),
           ),
         ],
