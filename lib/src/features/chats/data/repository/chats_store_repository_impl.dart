7a16bfe3a073703c38178c1e4fd56644e06b8a0e
kim kyugnwon <gwon080870@gmail.com>
2024-01-21 07:00:24 +0900

    FEAT: Add MyUid

--- lib/src/features/chats/data/repository/chats_store_repository_impl.dart
+++ lib/src/features/chats/data/repository/chats_store_repository_impl.dart
@@ -1,12 +1,15 @@
 import 'package:chat_application/src/core/constants/cloud_firestore_path.dart';
-import 'package:chat_application/src/features/chats/domain/model/user_info.dart';
+import 'package:chat_application/src/features/chats/domain/model/user_info.dart'
+    as Domain;
 import 'package:chat_application/src/features/chats/domain/repository/chats_store_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatsStoreRepositoryImpl extends ChatsStoreRepository {
   @override
-  Future<String> createChat(List<UserInfo> users) {
-    List<String> uids = users.map((e) => e.uid).toList();
+  Future<String> createChat(List<Domain.UserInfo> users) {
+    String myUid = FirebaseAuth.instance.currentUser!.uid;
+    List<String> uids = [myUid, ...users.map((e) => e.uid).toList()];
 
     return FirebaseFirestore.instance
         .collection(CloudFirestorePath.chats)
