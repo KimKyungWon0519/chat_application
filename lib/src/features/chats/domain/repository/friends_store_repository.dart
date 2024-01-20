e21065cde7d1a55d082423526af9fabf26b2619e
kim kyugnwon <gwon080870@gmail.com>
2024-01-21 05:22:19 +0900

    FEAT: Get all Friends on repository

--- lib/src/features/chats/domain/repository/friends_store_repository.dart
+++ lib/src/features/chats/domain/repository/friends_store_repository.dart
@@ -1,1 +1,5 @@
-abstract class FriendsStoreRepository {}
+import 'package:chat_application/src/features/friend_management/domain/model/user_info.dart';
+
+abstract class FriendsStoreRepository {
+  Future<UserInfo> getAllFriends(String uid);
+}
