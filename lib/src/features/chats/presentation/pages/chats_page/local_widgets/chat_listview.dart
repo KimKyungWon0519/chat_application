3bc9f755a1cbf04163959a9d156553ec92349b4f
kim kyugnwon <gwon080870@gmail.com>
2024-01-23 15:42:50 +0900

    FEAT: Push chatroom page

--- lib/src/features/chats/presentation/pages/chats_page/local_widgets/chat_listview.dart
+++ lib/src/features/chats/presentation/pages/chats_page/local_widgets/chat_listview.dart
@@ -1,7 +1,10 @@
+import 'package:chat_application/src/core/routes/app_path_contants.dart';
+import 'package:chat_application/src/core/routes/app_routes.dart';
 import 'package:chat_application/src/features/chats/domain/model/chat_info.dart';
 import 'package:chat_application/src/features/chats/presentation/presenter/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class ChatListView extends ConsumerWidget {
   const ChatListView({super.key});
@@ -29,6 +32,12 @@ class _ChatItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(chatInfo.name),
+      onTap: () => context.pushNamed(
+        ChatsSubRoutes.chatRoom.name,
+        pathParameters: {
+          ChatRoomParamter.chatID: chatInfo.id,
+        },
+      ),
     );
   }
 }
