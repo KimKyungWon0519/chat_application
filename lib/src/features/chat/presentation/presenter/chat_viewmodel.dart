cffb676a12a1045ea69ae69ca6893c97668def2e
kim kyugnwon <gwon080870@gmail.com>
2024-01-23 16:36:09 +0900

    FEAT: Use GetChatDataUseCase on viewmodel

--- lib/src/features/chat/presentation/presenter/chat_viewmodel.dart
+++ lib/src/features/chat/presentation/presenter/chat_viewmodel.dart
@@ -1,1 +1,9 @@
-class ChatViewModel {}
+import 'package:chat_application/src/features/chat/domain/usecase/chat_store_usecase.dart';
+
+class ChatViewModel {
+  final GetChatDataUseCase _getChatDataUseCase;
+
+  ChatViewModel({
+    required GetChatDataUseCase getChatDataUseCase,
+  }) : _getChatDataUseCase = getChatDataUseCase;
+}
