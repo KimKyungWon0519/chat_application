d84363f661aa9061ed7fad7703826fb9ab0bcafa
kim kyugnwon <gwon080870@gmail.com>
2024-01-23 16:43:27 +0900

    FEAT: Add Type

--- lib/src/features/chat/presentation/presenter/providers.dart
+++ lib/src/features/chat/presentation/presenter/providers.dart
@@ -1,3 +1,4 @@
+import 'package:chat_application/src/features/chat/presentation/presenter/chat_viewmodel.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-late final Provider chatProvider;
+late final Provider<ChatViewModel> chatProvider;
