aaa492b4f460f1e67a855c530604ab3b9450b020
kim kyugnwon <gwon080870@gmail.com>
2024-01-26 16:39:59 +0900

    FEAT: Create Chat Item

--- lib/src/features/chat/presentation/pages/chat_room_page/local_widget/chat_listview.dart
+++ lib/src/features/chat/presentation/pages/chat_room_page/local_widget/chat_listview.dart
@@ -36,64 +36,87 @@ class ChatListView extends ConsumerWidget {
                       ],
                     ),
                     for (Message message in data.message)
-                      StreamBuilder(
-                        stream: ref
-                            .read(chatProvider)
-                            .getUserNameSnapshot(message.uid),
-                        builder: (context, snapshot) {
-                          bool isMine =
-                              ref.read(chatProvider).isMyUid(message.uid);
-
-                          String data = snapshot.data ?? '';
-
-                          return Align(
-                            alignment: isMine
-                                ? Alignment.centerRight
-                                : Alignment.centerLeft,
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: Column(
-                                crossAxisAlignment: isMine
-                                    ? CrossAxisAlignment.end
-                                    : CrossAxisAlignment.start,
-                                children: [
-                                  Text(data),
-                                  Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Card(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(10),
-                                          child: Text(
-                                            message.commnet,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleMedium,
-                                          ),
-                                        ),
-                                      ),
-                                    ]..insert(
-                                        isMine ? 0 : 1,
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 5),
-                                          child: Text(message.time
-                                              .substring(0, '00:00'.length)),
-                                        ),
-                                      ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
+                      _ChatItem(message: message),
                   ],
                 ),
             ],
           ),
         );
       },
+    );
+  }
+}
+
+class _ChatItem extends ConsumerWidget {
+  final Message message;
+
+  const _ChatItem({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return StreamBuilder(
+      stream: ref.read(chatProvider).getUserNameSnapshot(message.uid),
+      builder: (context, snapshot) {
+        bool isMine = ref.read(chatProvider).isMyUid(message.uid);
+
+        String data = snapshot.data ?? '';
+
+        return Align(
+          alignment: isMine ? Alignment.centerRight : Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment:
+                  isMine ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              children: [
+                Text(data),
+                _ChatBubble(
+                  message: message,
+                  isMine: isMine,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ChatBubble extends StatelessWidget {
+  final Message message;
+  final bool isMine;
+
+  const _ChatBubble({
+    super.key,
+    required this.message,
+    required this.isMine,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              message.commnet,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+        ),
+      ]..insert(
+          isMine ? 0 : 1,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Text(message.time.substring(0, '00:00'.length)),
+          ),
+        ),
     );
   }
 }
